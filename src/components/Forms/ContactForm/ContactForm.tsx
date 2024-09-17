941d16f0a4e204f60d88475fcb2a3286a24a5f28
wellington cimento <wellingtoncimentooficial@gmail.com>
2024-09-17 01:14:40 -0300

    adding recaptcha badge again

--- src/components/Forms/ContactForm/ContactForm.tsx
+++ src/components/Forms/ContactForm/ContactForm.tsx
@@ -63,7 +63,7 @@ const ContactForm = (props: Props) => {
         initializeRecaptcha({
             sitekey: process.env.REACT_APP_RECAPTCHA_TOKEN ?? '', 
             container: "recaptcha", 
-            badge: "hidden",
+            badge: "default",
             callback: handleSendEmail
         })
     }, [initializeRecaptcha, handleSendEmail])
