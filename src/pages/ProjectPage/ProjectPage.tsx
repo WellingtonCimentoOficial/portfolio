1039fb715b426c2defeca99542be9093bea8d478
wellington cimento <wellingtoncimentooficial@gmail.com>
2024-09-21 00:28:21 -0300

    main modal responsiveness adjusted

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import styles from './ProjectPage.module.css'
 import { useParams } from 'react-router-dom'
 import { ProjectType } from '../../types/projectType'
 import { projectData } from '../../datas/projectsData'
+import GlideCarousel from '../../components/Carousels/GlideCarousel/GlideCarousel'
+import WidthLayout from '../../layouts/WidthLayout/WidthLayout'
 
 type Props = {}
 
@@ -19,7 +22,17 @@ const ProjectPage = (props: Props) => {
     }, [projectId])
 
     return (
-        <div>{project?.title}</div>
+        project && (
+            <div className={styles.wrapper}>
+                <WidthLayout>
+                    <div className={styles.container}>
+                        <section className={styles.sectionCarousel}>
+                            <GlideCarousel mainData={project.images.main} data={project.images.others} />
+                        </section>
+                    </div>
+                </WidthLayout>
+            </div>
+        )
     )
 }
 
